62bf2f15c310524a81d5e98a02e6c0ba759a71ba
kencarli <lizhim09@163.com>
2025-01-08 17:32:47 +0800

    Update googleTranslate.js

--- src/services/googleTranslate.js
+++ src/services/googleTranslate.js
@@ -1,1 +1,13 @@
+const axios = require('axios');
 
+const translateText = async (text, targetLang) => {
+  const apiKey = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
+  const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+  const response = await axios.post(url, {
+    q: text,
+    target: targetLang,
+  });
+  return response.data.data.translations[0].translatedText;
+};
+
+module.exports = { translateText };
